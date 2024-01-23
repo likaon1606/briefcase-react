b7e2f50a7d1e2c9f6000471b925cea0c9a5b7087
Marketing3 <ariel.fuentesgarcia@gmail.com>
2024-01-23 11:06:16 -0600

    corrections

--- src/components/contactMe/ContactMe.jsx
+++ src/components/contactMe/ContactMe.jsx
@@ -1,38 +1,38 @@
-import Images from '../home/Images';
-import Shelf from '../home/Shelf';
-import email from '../../assets/img/email.png';
-import phone from '../../assets/img/phone2.png';
-import whats from '../../assets/img/whatsapp.png';
-import conversation from '../../assets/img/conversation.png';
-import '../../styles/contactme/contactme.css';
+import Images from "../home/Images";
+import Shelf from "../home/Shelf";
+import email from "../../assets/img/email.png";
+import phone from "../../assets/img/phone2.png";
+import whats from "../../assets/img/whatsapp.png";
+import conversation from "../../assets/img/conversation.png";
+import "../../styles/contactme/contactme.css";
 
 const ContactMe = () => {
   return (
-    <div className='principal'>
+    <div className="principal">
       <Images />
-      <div className='about'>
-        <div className='container-contact'>
-          <div className='conversation'>
-            <img src={conversation} alt='conversation' />
+      <div className="about">
+        <div className="container-contact">
+          <div className="conversation">
+            <img src={conversation} alt="conversation" />
           </div>
-          <div className='contact-me'>
-            <img src={phone} alt='email' className='img-contactme' />
+          <div className="contact-me">
+            <img src={phone} alt="phone" className="img-contactme" />
             <h4>
-              <a href='tel:+525532604568'>(+52) 55 80 36 58 33</a>
+              <a href="tel:+525532604568">(+52) 3260 4568</a>
             </h4>
           </div>
-          <div className='contact-me'>
-            <img src={email} alt='email' className='img-contactme' />
+          <div className="contact-me">
+            <img src={email} alt="email" className="img-contactme" />
             <h4>
-              <a href='mailto:ariel.fuentesgarcia@gmail.com' target='blank'>
+              <a href="mailto:ariel.fg@mexwebdesign.com" target="blank">
                 ariel.fuentesgarcia@gmail.com
               </a>
             </h4>
           </div>
-          <div className='contact-me'>
-            <img src={whats} alt='email' className='img-contactme' />
+          <div className="contact-me">
+            <img src={whats} alt="whatsapp" className="img-contactme" />
             <h4>
-              <a href='mailto:ariel.fg@mexwebdesign.com' target='blank'>
+              <a href="https://wa.me/5532604568" target="blank">
                 55 3260 4568
               </a>
             </h4>
